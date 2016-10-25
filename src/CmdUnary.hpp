5430adf0c429c149e592323c98b4b5ce2d4d9f1c
Shindo <cn.shindo@gmail.com>
2016-10-24 21:21:22 -0700

    [BUGFIX] Missing constructor in CmdUnary and getter&setter for CmdUnary::executor.

--- src/CmdUnary.hpp
+++ src/CmdUnary.hpp
@@ -9,6 +9,20 @@ private:
     Executor* executor;
 
 public:
+    CmdUnary(std::string executable,
+             std::vector<std::string> argList)
+            : executable(executable), argList(argList)
+    {}
+
+    CmdUnary(std::string executable,
+             std::vector<std::string> argList,
+             Executor* executor)
+            : executable(executable), argList(argList), executor(executor)
+    {}
+
+    Executor* getExecutor() const { return executor; }
+    void setExecutor(Executor* executor) { this->executor = executor; }
+
     int execute() 
     {
         return executor->execute(executable, argList);
