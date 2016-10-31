22201d949bd6704414a233e126271c1779acb7aa
otanw001 <otanw001@ucr.edu>
2016-10-31 12:26:08 -0700

    [TASK] Updated commented_command test

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
+if [ ! -f "../bin/rshell" ]; then
+        echo "Run \`make\` first!"
+        exit 1
+fi
+
+echo "Execute echo hello #this is a comment"
+../bin/rshell << EOF
+echo hello #this is a comment
+echo
+EOF
+
+echo "Execute #echo hello"
+../bin/rshell << EOF
+#echo hello
+echo
+EOF
+
+echo "Execute #echo hello || echo hello2; mkdir blabla"
+../bin/rshell << EOF
+#echo hello || echo hello2; mkdir blabla
+echo
+EOF
+
+echo "Execute echo hello; echo hello || #echo hello; echo hello && echo hello"
+../bin/rshell << EOF
+echo hello; echo hello || #echo hello; echo hello && echo hello
+echo
+EOF
+
+echo "Execute echo hello && echo hello; echo hi && #echo bye"
+../bin/rshell << EOF
+echo hello && echo hello; echo hi && #echo bye
+echo
+EOF
+
+echo "Execute ls || echo hello && echo hello #hi && echo hello exit; #exit; echo hello"
+../bin/rshell << EOF
+ls || echo hello && echo hello #hi && echo hello exit; #exit; echo hello
+echo
+EOF
 
 exit 0
