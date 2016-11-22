aabd441df925ba7b7e9a083c6d548cc50fbb4fc1
Shindo <cn.shindo@gmail.com>
2016-11-21 21:27:41 -0800

    [TASK] Fix newline format.

--- tests/cd_test.sh
+++ tests/cd_test.sh
@@ -5,24 +5,36 @@ if [ ! -f "../bin/rshell" ]; then
 	exit 1
 fi
  
-echo "Execute cd cd"
+echo "Execute pwd; cd cd; pwd"
 ../bin/rshell << EOF
-cd cd 
+pwd; cd cd; pwd
 echo
 EOF
 
-echo "Execute pwd; mkdir random; cd random; pwd; cd; pwd"
+echo "Execute pwd; cd ../src; pwd"
 ../bin/rshell << EOF
-pwd; mkdir random; cd random; pwd; cd; pwd
+pwd; cd ../src; pwd
+echo
+EOF
+
+echo "Execute pwd; cd /usr/bin; pwd"
+../bin/rshell << EOF
+pwd; cd /usr/bin; pwd
+echo
+EOF
+
+echo "Execute pwd; mkdir random; cd random; pwd; cd -; pwd"
+../bin/rshell << EOF
+pwd; mkdir random; cd random; pwd; cd -; pwd
 echo
 EOF
 
 #clean up directory 'random'
 rm -rf random
 
-echo "Execute pwd; mkdir hello; cd hello; pwd"
+echo "Execute pwd; cd; pwd; cd -; pwd"
 ../bin/rshell << EOF
-pwd; mkdir hello; cd hello; pwd
+pwd; cd; pwd; cd -; pwd
 echo
 EOF
 
@@ -41,9 +53,15 @@ pwd; mkdir hello; cd hello; pwd; mkdir hello1; cd hello1; pwd
 echo
 EOF
 
+#clean up directory 'hello'
+rm -rf hello
+
+#clean up directory 'hello1'
+rm -rf hello1
+
 echo "Execute pwd; mkdir hello; cd hello; pwd; mkdir hello1; cd hello1; pwd; cd..; pwd; cd ..; pwd"
 ../bin/rshell << EOF
-pwd; mkdir hello; cd hello; pwd; mkdir hello1; cd hello1; pwd; cd..; pwd; cd ..; pwd
+pwd; mkdir hello; cd hello; pwd; mkdir hello1; cd hello1; pwd; cd ..; pwd; cd ..; pwd
 echo
 EOF
 
@@ -65,9 +83,9 @@ rm -rf hello
 #clean up directory 'hello1'
 rm -rf hello1
 
-echo "Execute mkdir hello; cd hello; mkdir hello1; cd hello1; cd..; cd..; cd ./hello/hello1"
+echo "Execute pwd; mkdir hello; cd hello; pwd; mkdir hello1; cd hello1; pwd; cd ..; pwd; cd ..; pwd; cd hello/hello1; pwd"
 ../bin/rshell << EOF
-mkdir hello; cd hello; mkdir hello1; cd hello1; cd..; cd..; cd ./hello/hello1
+pwd; mkdir hello; cd hello; pwd; mkdir hello1; cd hello1; pwd; cd ..; pwd; cd ..; pwd; cd hello/hello1; pwd
 echo
 EOF
 
@@ -77,9 +95,9 @@ rm -rf hello
 #clean up directory 'hello1'
 rm -rf hello1
 
-echo "Execute mkdir hello; cd hello; mkdir hello1; cd hello1; cd..; cd..; cd /hello/hello1"
+echo "Execute pwd; mkdir hello; cd hello; pwd; mkdir hello1; cd hello1; pwd; cd ..; pwd; cd ..; pwd; cd /hello/hello1 pwd"
 ../bin/rshell << EOF
-mkdir hello; cd hello; mkdir hello1; cd hello1; cd..; cd..; cd /hello/hello1
+Execute pwd; mkdir hello; cd hello; pwd; mkdir hello1; cd hello1; pwd; cd ..; pwd; cd ..; pwd; cd /hello/hello1 pwd
 echo
 EOF
 
